905cf9392a00a5ccd0aafaccabb872e3671191b5
Alex Couture-Beil <alex@mofo.ca>
2023-09-21 03:39:29 +0000

    stuff

--- water.py
+++ water.py
@@ -13,39 +13,91 @@ import sys
 # ordered from relay 1 to 8
 pins = [25, 10, 24, 23, 22, 27, 17, 18]
 
+debug_loop=False
+
 zones = {
-    'greenhouse': pins[0], # yellow
-    'unused_1': pins[1], # green
-    'unused_2': pins[2], # orange
-    'unused_3': pins[3], # blue
-    'unused_4': pins[4], # brown
-    'unused_5': pins[5], # red
+    'greenhouse': 'yellow',
+    'front': 'green',
+    'frontside': 'blue',
+    'unused_4': 'brown',
+    'unused_5': 'red',
     }
+
+colorpins = {
+    'yellow': pins[0],
+    'green': pins[1],
+    'dead_relay': pins[2],
+    'blue': pins[3],
+    'brown': pins[4],
+    'red': pins[5],
+    'orange': pins[6],
+}
+print(colorpins)
+
+def get_pin(s):
+    if s in zones:
+        return colorpins[zones[s]]
+    if s in colorpins:
+        return colorpins[s]
+    return None
+
+def help():
+    print('usage: water.py <zone> <num_minutes> | stop')
+    sys.exit(1)
 
 def main():
     setup_logging()
-    logging.info(f'water scheduler started')
+
+    stop = False
+    if len(sys.argv) == 2:
+        if sys.argv[1] == 'stop':
+            stop = True
+        else:
+            help()
+    elif len(sys.argv) != 3:
+        help()
+
+    if stop:
+        print('stopping all zones')
+        GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+        for p in pins:
+            GPIO.setup(p, GPIO.OUT)
+            GPIO.output(p, GPIO.HIGH) # should be default, but add here to be safe
+        sys.exit(0)
+
+    zone = sys.argv[1]
+    num_minutes = int(sys.argv[2])
+    duration_seconds = num_minutes*60
+
+    pin = get_pin(zone)
+    if pin is None:
+        print(f'invalid zone {zone}')
+        sys.exit(1)
+
+    logging.info(f'water scheduler started; {zone} (pin {pin}) for {num_minutes}')
     GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
-    for pin in pins:
-        GPIO.setup(pin, GPIO.OUT)
-        GPIO.output(pin, GPIO.HIGH) # should be default, but add here to be safe
+    for p in pins:
+        GPIO.setup(p, GPIO.OUT)
+        GPIO.output(p, GPIO.HIGH) # should be default, but add here to be safe
     
-    zone = 'greenhouse'
-    pin = zones['greenhouse']
     try:
         while 1:
-            now = datetime.datetime.now()
-            logging.info(f'turning {zone} on')
+            logging.info(f'turning {zone} (pin {pin}) on')
             GPIO.output(pin, GPIO.LOW) # turn water on
-            time.sleep( 10*60 )
-            logging.info(f'turning {zone} off')
+            time.sleep( duration_seconds )
+            logging.info(f'turning {zone} (pin {pin}) off')
             GPIO.output(pin, GPIO.HIGH) # turn water off
-            #time.sleep( 50 )
+            if debug_loop:
+                time.sleep( duration_seconds )
+                continue
             break
     except BaseException as e:
         logging.info(f'unexpected {e=}')
         raise
     finally:
+        logging.info(f'turning {zone} off')
+        GPIO.output(pin, GPIO.HIGH) # turn water off just in case
+
         GPIO.cleanup()
         logging.info(f'water scheduler exiting')
 
